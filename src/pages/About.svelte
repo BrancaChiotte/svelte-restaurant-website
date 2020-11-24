df10deb2a130375a9434313d92f8381e1641b1d3
Branca Chiotte <brancachiotte@gmail.com>
2020-11-24 10:52:53 +0000

    first commit

---
+++ src/pages/About.svelte
@@ -0,0 +1,14 @@
+<div id="about" class="about-content">
+  <section class="section about-section">
+    <h1 class="section-title">about us</h1>
+    <div class="products-center">
+      <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+      repudiandae architecto qui adipisci in officiis, aperiam sequi atque
+      perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
+      nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione hic
+      aspernatur error blanditiis?
+    </p>
+    </div>
+  </section>
+</div>
